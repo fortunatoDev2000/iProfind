2aed938d0c69f2c9e55b7c33a84466d6f894ba30
fortunatoDev2000 <fortunatodev2000@gmail.com>
2023-06-30 09:05:40 -0300

    adiciona o fade-out na janela de seleção de localização, exclui algumas linhas vazias de codigo.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.getElementById('abrir-janela-local').addEventListener('click', function() {
     document.getElementById('janela-local').classList.remove('fechada');
+    document.getElementById('janela-local').classList.remove('fechadapos');
+    document.getElementById('janela-local').classList.add('aberta');
   });
 
   document.getElementById('janela-local').addEventListener('click', function(event) {
     if (event.target === this) {
-      this.classList.add('fechada');
+      this.classList.add('fechadapos');
+      this.classList.remove('aberta');
     }
   });
   
   document.getElementById('fechar-janela-local').addEventListener('click', function() {
-    document.getElementById('janela-local').classList.add('fechada');
+    document.getElementById('janela-local').classList.add('fechadapos');
+    document.getElementById('janela-local').classList.remove('aberta');
   });
