a0b43e7337b52689d8fda1f553278f778b92898e
fortunatoDev2000 <fortunatodev2000@gmail.com>
2023-06-30 02:25:51 -0300

    corrige alguns bugs e adiciona uma animação de fade-in na janela de seleção da localização

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('abrir-janela-local').addEventListener('click', function() {
     document.getElementById('janela-local').classList.remove('fechada');
   });
-  
+
   document.getElementById('janela-local').addEventListener('click', function(event) {
     if (event.target === this) {
       this.classList.add('fechada');
